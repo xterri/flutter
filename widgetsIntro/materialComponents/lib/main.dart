ad92afbb80d4bb04e8c5d8ae19b82bbf4eb84e05
Teresa Huynh <thuynh1989@gmail.com>
2020-10-20 21:07:22 -0600

    stateful widget

--- widgetsIntro/materialComponents/lib/main.dart
+++ widgetsIntro/materialComponents/lib/main.dart
@@ -29,7 +29,13 @@ class TutorialHome extends StatelessWidget {
       ),
       // body = majority of the screen
       body: Center(
-        child: Button(),
+        child: Row(
+          children: <Widget>[
+            Button(),
+            Text("Don't Press the Button"),
+            Counter(),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Add',
@@ -61,6 +67,44 @@ class Button extends StatelessWidget {
           child: Text('Press Me'),
         ),
       ),
+    );
+  }
+}
+
+// Stateful Widgets - Changing widgets in response to input
+
+// use "stful" shortcut to create Stateful Widget templates
+class Counter extends StatefulWidget {
+  // this class = configuration for state
+  // holds the values given by parent and used to build method of State
+  // Fields in Widget subcass are always marked "final" ?
+
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      // call setState to tell Flutter framework smth has changed
+      // this call causes framework to rerun the build method to reflect updated values
+      // if _counter change w/o call to setState() = build method won't rerun, updates don't change
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: _increment,
+          child: Text('Add more'),
+        ),
+        Text('Count: $_counter'),
+      ],
     );
   }
 }
